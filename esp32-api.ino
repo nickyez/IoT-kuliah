0fecc4edd682c562bc3023c4e834578f599274c3
Nicky Erlangga Setiawan <44841392+NickyErlanggaZ@users.noreply.github.com>
2021-04-01 10:03:20 +0700

    Memasukkan variabel namaPengguna

--- esp32-api.ino
+++ esp32-api.ino
@@ -4,8 +4,10 @@ int randNumber;
 const char* ssid = "RUMAH"; // Nama wifi kalian
 const char* password = "11111111"; //Password Kalian
 String serverName = "http://139.162.44.93/insert_data.php/"; // alamat server kita
+String namaPengguna = "NickyErlangga" // Nama kalian tanpa tanda spasi
 unsigned long lastTime = 0;
 unsigned long timerDelay = 5000;
+
 void setup() {
     Serial.begin(115200);
     WiFi.begin(ssid, password);
@@ -20,6 +22,7 @@ void setup() {
     Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing
     the first reading.");
 }
+                   
 void loop() {
     //Send an HTTP POST request every 10 minutes
     if ((millis() - lastTime) > timerDelay) {
@@ -27,7 +30,7 @@ void loop() {
         if(WiFi.status()== WL_CONNECTED){
             HTTPClient http;
             randNumber = random(100);
-            String serverPath = serverName + "?&nama_sensor=SensorDHT11&nilai=" + randNumber + "&user=NickyErlangga"; // user ini sesuai nama kalian, tanpa spasi ya!!
+            String serverPath = serverName + "?&nama_sensor=SensorDHT11&nilai=" + randNumber + "&user=" + namaPengguna; 
             // Your Domain name with URL path or IP address with path
             http.begin(serverPath.c_str());
             // Send HTTP GET request
