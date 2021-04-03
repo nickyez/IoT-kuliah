a33b36a0c74c62ef854fc39f0bf7687e41df187f
Nicky Erlangga Setiawan <44841392+NickyErlanggaZ@users.noreply.github.com>
2021-04-03 15:56:25 +0700

    Debug serial println error

--- esp32-api.ino
+++ esp32-api.ino
@@ -19,8 +19,7 @@ void setup() {
     Serial.println("");
     Serial.print("Connected to WiFi network with IP Address: ");
     Serial.println(WiFi.localIP());
-    Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing
-    the first reading.");
+    Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
 }
                    
 void loop() {
